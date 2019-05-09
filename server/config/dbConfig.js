ec4b08e28ce48d670c18b5babe00ef24c79fa371
Thiên Trương <tdthien1997@gmail.com>
2019-05-09 18:10:02 +0700

    Add remote database connection string

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: 'mongodb://localhost:27017/BookLenderSystem'
+  url: 'mongodb://localhost:27017/BookLenderSystem',
+  remoteUrl: 'mongodb://booklender:mKngvanAgC7Ssf5cWU9PHfjfy64WTQ6k28nfwhfOhzE1GtiWoU8H5lofdgX7Ha5flJKfptX8Ykw6iVf6g98lBA==@booklender.documents.azure.com:10255/?ssl=true'
 }
